266315e373198c5ca32d32b0d90fdcb1baa6e2a9
jenna <jenna.berlinberg@gmail.com>
2020-02-08 23:58:17 -0800

    Grammaring and testing galore

--- grammar/__tests__/grammar_errors.test.js
+++ grammar/__tests__/grammar_errors.test.js
@@ -8,15 +8,7 @@
 const syntaxCheck = require('../syntax-checker');
 
 const errors = [
-  ['keyword as id', 'else := 5'],
-  ['unclosed paren', 'let var x := (2 * 3 in end'],
-  ['unknown operator', 'x := 2 ** 5'],
-  ['chained relational operators', '1 < 3 < 5'],
-  ['unclosed comment', 'x := /*   hello 9'],
-  ['bad unicode escape', '"ab\\u{1f4%a9}c"'],
-  ['bad escape', '"ab\\q"'],
-  ['bad character in id', '$x := 1'],
-  // TODO: We need dozens more here....
+  ['rogue else', 'else x > 1: break']
 ];
 
 describe('The syntax checker', () => {
