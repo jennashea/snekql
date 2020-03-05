79cd5e126acbada4bb95802c864c7cf0805877ae
jenna <jenna.berlinberg@gmail.com>
2020-03-05 14:17:18 -0800

    Separate error tests

--- grammar/__tests__/grammar_errors.test.js
+++ grammar/__tests__/grammar_errors.test.js
@@ -7,8 +7,7 @@
 
 const syntaxCheck = require('../syntax-checker');
 
-const errors = [
-  ['rogue else', 'else x > 1: break'],
+const operationErrors = [
   ['used keyword as variable name', 'fnc = 10'],
   ['rogue semicolon', 'print("hi");'],
   [
@@ -20,13 +19,7 @@ if x:
   ],
   ['improper array', 'myArray[if a > b: hiss("yay")]'],
   ['improper array 2', 'myArray[for something in data: hiss(something)]'],
-  [
-    'no semi-colon',
-    `if c > 10
-      return 0`
-  ],
   ['incorrect binary expression', '3///4'],
-  ['lack of continuation from statement','if 10:'],
   ['bad call', 'hiss(10'],
   ['bad comparator', '10 =< 2'],
   ['bad reassign','a =* 10'],
@@ -35,6 +28,10 @@ if x:
   ['bad array 3','x = 2, 3, 4]'],
   ['bad array 4','x = [2, 3, 4'],
   ['bad array 5','x = 2, 3, 4'],
+];
+
+loopErrors = [
+  ['rogue else', 'else x > 1: break'],
   ['else without if', 'else: hiss(2)'],
   ['elif without if', 'elif x > 3: hiss(2)'],
   [
@@ -45,14 +42,32 @@ if x:
       hiss(2)
     elif x > 3:
       hiss(3)`
-  ]
+  ],
+  [
+    'no colon',
+    `if c > 10
+      return 0`
+  ],
+  ['lack of continuation from statement','if 10:']
 ];
 
 describe('The syntax checker', () => {
-  errors.forEach(([scenario, program]) => {
+  operationErrors.forEach(([scenario, program]) => {
     test(`detects the error ${scenario}`, done => {
       expect(syntaxCheck(program)).toBe(false);
       done();
     });
   });
+  loopErrors.forEach(([scenario, program]) => {
+    test(`detects the error ${scenario}`, done => {
+      expect(syntaxCheck(program)).toBe(false);
+      done();
+    });
+  });
+  // errors.forEach(([scenario, program]) => {
+  //   test(`detects the error ${scenario}`, done => {
+  //     expect(syntaxCheck(program)).toBe(false);
+  //     done();
+  //   });
+  // });
 });
