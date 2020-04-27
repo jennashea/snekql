9500be702d37ef50331f09aa7802f16d5640675b
alvinlai808 <35281838+alvinlai808@users.noreply.github.com>
2020-04-26 15:54:15 -1000

    Rough generator shell

---
+++ backend/javascript-generator.js
@@ -0,0 +1,143 @@
+const {
+  Program,
+  BinaryExp,
+  Literal,
+  IdExp,
+  Print,
+  Assignment,
+  NegationExp,
+  WhileExp,
+  Suite,
+  ForExp,
+  Call,
+  Argument,
+  Null,
+  Member,
+  SubscriptedRangeable,
+  IfStmt,
+  Break,
+  Rule,
+  Arr,
+  FunctionDeclaration,
+  ArrayType,
+  Types,
+  Param,
+  Params,
+  Return,
+  VariableDeclaration,
+} = require("../ast");
+const {
+  IntType,
+  StringType,
+  DoubleType,
+  BooleanType,
+  ArrayOfType,
+  ArrayOfIntType,
+  ArrayOfStringType,
+  ArrayOfBooleanType,
+  ArrayOfDoubleType,
+} = require("../semantics/builtins");
+
+Program.prototype.gen = function() {
+  return
+};
+
+BinaryExp.prototype.gen = function() {
+  return
+};
+
+Literal.prototype.gen = function() {
+  return
+};
+
+IdExp.prototype.gen = function() {
+  return
+};
+
+Print.prototype.gen = function() {
+  return
+};
+
+Assignment.prototype.gen = function() {
+  return
+};
+
+NegationExp.prototype.gen = function() {
+  return
+};
+
+WhileExp.prototype.gen = function() {
+  return
+};
+
+Suite.prototype.gen = function() {
+  return
+};
+
+ForExp.prototype.gen = function() {
+  return
+};
+
+Call.prototype.gen = function() {
+  return
+};
+
+Argument.prototype.gen = function() {
+  return
+};
+
+Null.prototype.gen = function() {
+  return
+};
+
+Member.prototype.gen = function() {
+  return
+};
+
+SubscriptedRangeable.prototype.gen = function() {
+  return
+};
+
+IfStmt.prototype.gen = function() {
+  return
+};
+
+Break.prototype.gen = function() {
+  return
+};
+
+Rule.prototype.gen = function() {
+  return
+};
+
+Arr.prototype.gen = function() {
+  return
+};
+
+FunctionDeclaration.prototype.gen = function() {
+  return
+};
+
+ArrayType.prototype.gen = function() {
+  return
+};
+
+Types.prototype.gen = function() {
+  return
+};
+
+Param.prototype.gen = function() {
+  return
+};
+
+Params.prototype.gen = function() {
+  return
+};
+
+Return.prototype.gen = function() {
+  return
+};
+
+VariableDeclaration.prototype.gen = function() {
+  return
+};
