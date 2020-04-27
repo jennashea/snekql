f1941529bd5a83db234bf8d3acb0668fa1ac91be
alvinlai808 <35281838+alvinlai808@users.noreply.github.com>
2020-04-26 22:54:13 -1000

    Test for printing strings

--- backend/__tests__/javascript.test.js
+++ backend/__tests__/javascript.test.js
@@ -9,6 +9,13 @@ const parse = require('../../ast/parser');
 const analyze = require('../../semantics/analyzer');
 const generate = require('../javascript-generator');
 
+const fixture = {
+  hello: [
+    String.raw`hiss("Hello, world\n")`, 
+    String.raw`console.log("Hello, world\n")`
+  ]
+};
+
 describe('The JavaScript generator', () => {
   Object.entries(fixture).forEach(([name, [source, expected]]) => {
     test(`produces the correct output for ${name}`, done => {
