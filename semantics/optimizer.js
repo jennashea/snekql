4742ffc90c0575b2cb415f9c291575171cd22a92
Jialing Li <Linannan0505@gmail.com>
2020-04-28 16:20:13 -0400

    filled all skeleton to the best of my knowledge

--- semantics/optimizer.js
+++ semantics/optimizer.js
@@ -27,17 +27,17 @@ const {
 
   module.exports = program => program.optimize();
 
-//   function isZero(e) {
-//     return e instanceof Literal && e.value === 0;
-//   }
+  function isZero(e) {
+    return e instanceof Literal && e.value === 0;
+  }
   
-//   function isOne(e) {
-//     return e instanceof Literal && e.value === 1;
-//   }
+  function isOne(e) {
+    return e instanceof Literal && e.value === 1;
+  }
   
-//   function bothLiterals(b) {
-//     return b.left instanceof Literal && b.right instanceof Literal;
-//   }
+  function bothLiterals(b) {
+    return b.left instanceof Literal && b.right instanceof Literal;
+  }
 
 Assignment.prototype.optimize = function() {
     this.target = this.target.optimize();
@@ -65,7 +65,7 @@ Assignment.prototype.optimize = function() {
   };
   
   Call.prototype.optimize = function() {
-    this.args = this.args.map(a => a.optimize());
+    this.args = this.args.map(arg => arg.optimize());
     this.callee = this.callee.optimize();
     return this;
   };
@@ -97,79 +97,103 @@ Assignment.prototype.optimize = function() {
   };
 
   Param.prototype.optimize = function() {
-      //?
     return this;
   };
 
   WhileExp.prototype.optimize = function() {
     this.test = this.test.optimize();
-    if (this.test instanceof Literal && !this.test.value) {
-      return new Nil();
-    }
+    // if (this.test instanceof Literal && !this.test.value) {
+    //   return new Nil();
+    // }      // this needs to be changed to the boolean way instead of integer
     this.body = this.body.optimize();
     return this;
   };
 
   Program.prototype.optimize = function() {
-
+    this.statements = this.statements.map(s => s.optimize());
+    return this;
   };
 
   BinaryExp.prototype.optimize = function() {
+    this.left = this.left.optimize();
+    this.right = this.right.optimize();
+    if (this.op === '+' && isZero(this.right)) return this.left;
+    if (this.op === '+' && isZero(this.left)) return this.right;
+    if (this.op === '*' && isZero(this.right)) return new Literal(0);
+    if (this.op === '*' && isZero(this.left)) return new Literal(0);
+    if (this.op === '*' && isOne(this.right)) return this.left;
+    if (this.op === '*' && isOne(this.left)) return this.right;
 
+    // if (bothLiterals(this)) {
+    //   const [x, y] = [this.left.value, this.right.value];
+    //   if (this.op === '+') return new Literal(x + y);
+    //   if (this.op === '*') return new Literal(x * y);
+    //   if (this.op === '/') return new Literal(x / y);
+    // }
+    //this above needs to be changed since we are using boolean.
+    return this;
   };
 
   Print.prototype.optimize = function() {
-
+    this.expression = this.expression.optimize();
   };
 
   Suite.prototype.optimize = function() {
-
+    this.stmt = this.stmt.map(s => s.optimize());
+    return this;
   };
 
   Argument.prototype.optimize = function() {
-
+    this.expression = this.expression.optimize();
+    return this;
   };
 
-  SubscriptedRangeable.prototype.optimize = function() {
+  // SubscriptedRangeable.prototype.optimize = function() {
 
-  };
+  // };   //I think we don't need this either (?)
 
   IfStmt.prototype.optimize = function() {
-
+    this.firstCondition = this.firstCondition.optimize();
+    this.firstSuite = this.firstSuite.optimize();
+    this.potentialConditions = this.potentialConditions.map((condition, i) => condition.optimize());
+    this.potentialBlocks[i] = this.potentialBlocks[i].optimize();
+    this.elseCaseSuite = this.elseCaseSuite.optimize();
+    return this;
   };
 
   Rule.prototype.optimize = function() {
-
+    this.expressions = this.expressions.optimize();
+    return this;
   };
 
   Arr.prototype.optimize = function() {
-
+    this.expressions = this.expressions.map(e => e.optimize());
+    return this;
   };
 
   FunctionDeclaration.prototype.optimize = function() {
-
+    this.suite = this.suite.optimize();
+    return this;
   };
 
-  ArrayType.prototype.optimize = function() {
+  // ArrayType.prototype.optimize = function() {
       
-  };
+  // }; //I think we don't need this (?)
 
-  Types.prototype.optimize = function() {
+  // Types.prototype.optimize = function() {
 
-  };
-
-  Param.prototype.optimize = function() {
-
-  };
+  // }; //I think we don't need this (?)
 
   Params.prototype.optimize = function() {
-
+    this.parameters = this.parameters.map(s => s.optimize());
+    return this;
   };
 
   Return.prototype.optimize = function() {
-
+    return this;
   };
 
   VariableDeclaration.prototype.optimize = function() {
-
+    this.optionalSource = this.optionalSource.optimize();
+    return this;
   };
